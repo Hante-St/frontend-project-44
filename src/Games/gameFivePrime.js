c734ec0651294ec070e43efb63f76f69af8f796a
Hante-St <stepanenkova.katherine@gmail.com>
2025-06-21 16:48:30 +0300

    Game Five fixed

--- src/Games/gameFivePrime.js
+++ src/Games/gameFivePrime.js
@@ -1,19 +1,24 @@
-import runGame from '../Utils/index.js'
-import getRandomInt from '../Utils/getRandomInt.js'
+import runGame from "../Utils/index.js";
+import getRandomInt from "../Utils/getRandomInt.js";
 
-const gameDescription = 'Answer "yes" if given number is prime. Otherwise answer "no".'
+const gameDescription =
+  'Answer "yes" if given number is prime. Otherwise answer "no".';
 
 const Prime = (number) => {
   const question = `${number}`
-  let correctAnswer = 0
-  if (number >= 2 && number % 2 !== 0 && number % 3 !== 0) {
-    correctAnswer = 'yes'
-  }
-  else {
-    correctAnswer = 'no'
-  }
+  let correctAnswer = 'yes'
+  if (number < 2) {
+    correctAnswer = "no"
   return [question, correctAnswer]
 }
+  for (let i = 2; i <= Math.sqrt(number); i++) {
+    if (number % i === 0) {
+      correctAnswer = "no"
+      break
+    }
+  }
+  return [question, correctAnswer]
+};
 
 const gameFive = () => {
   const number = getRandomInt(0, 100)
